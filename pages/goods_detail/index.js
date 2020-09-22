92a27db4c4848b44d79b3b5b6eee7eddafcf910c
dayA <xbz1604@163.com>
2020-09-22 19:01:45 +0800

    添加选中状态

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -6,9 +6,10 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        goodsObj:{}
     },
 
+    GoodsInfo:{},
     /**
      * 生命周期函数--监听页面加载
      */
@@ -22,6 +23,7 @@ Page({
     /* 获取商品详情 */
     async  getGoodsDetail(goods_id){
         const res = await request({url:'/goods/detail',data:{goods_id}})
+        this.GoodsInfo = res.data.message
         this.setData({
             goodsObj:{
                 goods_name:res.data.message.goods_name,
@@ -41,6 +43,28 @@ Page({
             current: current, // 当前显示图片的http链接
             urls: urls // 需要预览的图片http链接列表
           })
+    },
+
+    /* 加入购物车 */
+    handleCartAdd(){
+        // 获取缓存中的购物车 数组
+        let cart = wx.getStorageSync('cart') || [];
+        // 判断 商品对象是否存在于购物车数组中
+        let index  = cart.findIndex(v=>v.goods_id === this.GoodsInfo.goods_id)
+        if(index === -1){
+            // 不存在
+            this.GoodsInfo.num = 1;
+            this.GoodsInfo.checked = true;
+            cart.push(this.GoodsInfo)  
+        }else{
+            cart[index].num++;
+        }
+        wx.setStorageSync('cart', cart);
+        wx.showToast({
+            title: '加入成功',
+            icon: 'success',
+            mask: true
+        });
     },
     /**
      * 生命周期函数--监听页面初次渲染完成
